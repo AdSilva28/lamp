d2898d895335207e7b22a48f579966eafa58db2d
Adilson Silva <106982898+AdSilva28@users.noreply.github.com>
2022-11-05 16:32:17 -0300

    Update lamp.js
    Apenas inserção de alguns comentários para explicar o código

--- lamp.js
+++ lamp.js
@@ -1,7 +1,7 @@
-var lamp = window.document.getElementById("lamp")
-var info = document.getElementById("info")
-var lamp_acts = document.getElementById("lamp_acts")
-var button = document.getElementById("button")
+var lamp = window.document.getElementById("lamp") //var criada apenas para quebrar a lâmapada
+var info = document.getElementById("info") //var para informar a situação atual da lâmpada
+var lamp_acts = document.getElementById("lamp_acts") //var relacionada à ações da lâmpada de ligar e desligar
+var button = document.getElementById("button") //var relacionada a ações do switch
 
 function ligar() {
     info.innerHTML = "Lâmpada ligou!"
@@ -22,6 +22,10 @@ function quebrar() {
     button.src = "./img/switch_off.png"
 }
 
+//adicionados dois listeners para eventos no switch da lâmpada
+//detalhe: como não sei usar dois "clicks" para o mesmo item eu coloquei um clique duplo para diferenciar ações no switch
 button.addEventListener("click", ligar)
 button.addEventListener("dblclick", desligar)
+
+//adicionado um listener para um único evento na lâmpada
 lamp.addEventListener("click", quebrar)
